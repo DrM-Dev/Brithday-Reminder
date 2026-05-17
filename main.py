bdec762337554edd3b984543ee9dc15509d8f675
Dr.M Dev <ms.dental.business@gmail.com>
2026-05-17 12:09:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,129 @@
-import smtplib
+#Happy Birthday - ver       by      Dr.M-Dev
+ver = "0.1.1"
+#====================IMPORTS:
+from tkinter import *
+import customtkinter
+from PIL import ImageTk, Image
+#
+import ctk_gif_class
+from customtkinter import CTkImage, CTkLabel
+#
+from tkinter import messagebox
+#
+import random
+#----NEW:
+import sys
+import os
 
 
-#sender:
-my_email = r"shysamsciencechannels@gmail.com"
-my_email_pass = r"wfprafawhfqmmzyk" #<----------activate 2 steps authentication then App-Password to get this pass
-#reciver:
-yahoo_friend = r"drmpythontestingground@yahoo.com"
 
-#=============================
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    #
-    connection.login(user=my_email,password=my_email_pass)
-    connection.sendmail(from_addr=my_email,to_addrs=yahoo_friend, msg="Subject: henlooo\n\nTHIS IS AN SMTP TEST!\n :P")
+#====================STATES-LIST
+state_keys_list = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
-#=============================
-# Transport Layer Security (TLS)
+
+#====================Font/Colors Constants:
+BACKGROUND_COLOR = "#4f97fb"
+# GUESS_TITLE_FONT = "Ariel", 15, "italic"
+# STATE_NAME_FONT = "Courier", 50, "bold"
+
+#====================
+# States Left To Guess Counter
+cards_left = 50
+
+#====================Globals:
+player_choice = None
+player_SCORE = 0
+score_effected = False #(false -> the score is NOT YET Effected),
+                      # (true -> the score have already been effected, and shouldn't be altered any further)
+#
+# fetched_tuple = None
+the_state = "state_null"
+the_name = ""
+#
+lang_keys_list = []
+guessed_keys_list = []
+
+#====================SETUP
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+#-------------
+root = customtkinter.CTk()
+root.configure(fg_color=BACKGROUND_COLOR)
+#
+window_width = 1000
+window_height = 600
+#
+root.minsize(window_width,window_height)
+root.maxsize(window_width,window_height)
+root.config(padx=20,pady=20)
+#-------------
+root.title(f"States Flashcards Game {ver}")
+#----bitmap
+root.iconbitmap("images/StatesFlashGame_bitmap.ico")
+#----logo:
+logo = customtkinter.CTkImage(light_image=Image.open("images/LOGO_T_Black.png"),size=(110,100))
+logo_label = customtkinter.CTkLabel(root ,text="", fg_color="transparent" ,image=logo, bg_color="transparent")
+logo_label.place(x=5,y=180)
+#--------------------------
+#-------------Widgets displacement
+widgets_x_place = 20
+widgets_y_place = 20
+#|
+buttons_x_displacement = 50
+buttons_y_displacement = 50
+
+#====================Globals:
+#x+x+x+x+x+x+x+x+x+x+x+x
+chosen_state_title = ""
+
+#####################FLAG GIF:
+flag_gif = ctk_gif_class.CTkGIFLabel(root,gif_path="images/usa_flag_gif.gif") #200x100 is ideal + #no need to start animation, it's part of its __init__ implementation
+flag_gif.place(x=window_width/4+590,y=window_height/4+30)
+
+
+#______________________________________________________________
+print('''                                                                                                                                                  
+                                                              ...::::.      ...::::::::    :.      .:.   
+  5@@@@@@@@B!    &@@@@@@@&G:        ^G&@@@&P#@@@@B~          J@@@@@@@@@G.   #@@@@@@@@@@   .@@B    7@@?   
+  G@@~::::J@@!   @@#     B@@.      :@@G::~&@@!::Y@@~         J@@~    ^@@B   #@@.           !@@J  .@@B    
+  G@@     .@@Y   @@@    5&@#       ~@@!   B@&   :@@?         J@@:     &@#   #@@BBBBBBB      P@@: #@@.    
+  7BP     .@@J   PBGGGGGB@@B       :BB^   B@&   :@@?         ~GP.     &@#   JGPYYYYYYY       &@# @@!     
+  Y&&^....?@@7   #&P     J@@:  ##  ^&&~   B@&   ^@@?         ?@@7:  :7@@P   Y@& ......       ^@@@@P      
+  P@@@@@@@@&?    &@B     ?@@:  ##  ~@@!   B@&   :@@?         ?@@@@@@@@#J    J&@@@@@@@@?       ?@@B  
+
+
+                                                             !J!:                                                                
+                                                              ^G@@&P7:                                                           
+                                         .~7YGB#&&&&&&&#BG5?~:  .Y@@@@&G^                                                        
+                                    :?P&@@@@@@@@@@@@@@@@@@@@@@@&G?J@@@@@&                                                        
+                                .!G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@P   ...                                                 
+                              ~B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BG&&@@@@                                              
+                            ?&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                                             
+                          7@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GYP#&J                                            
+                        .B@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!                                                
+                       :&@@@@@@@@@@@@@@@@J7@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@P                                               
+                      .@@@@@@@@@@@@@@@@#:  ^&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@G                                              
+                      #@@@@@@@@@@@@@@&7      P@@@@@@@@@@@@@B&@@@@@@@&@@#&@@@@@@@@@@J                                             
+                     !@@@@@@@@@@@@@&?         ^#@@@@&&@@@@@@#PPGB##? B@5#@#@@@@@@@@@:                                            
+                     B@@@@@@@@@@@G~             ^B@@@&GG#@@@@@@@#~   .&#J@Y&@@@@@@@@G                                            
+                     @@@@@@@@@@~                  .?#@@@&BGPGBBJ      .#5G&J@@@@@@@@@.                                           
+                   .@@@@@@@@@7      !PB##B4^        .^JG#&&P:  ^4B###P4?!~!?@@@@@@@@^                                           
+                   .@@@@@@@@#      !4~.. .~4^                 ~4~....~4^    #@@@@@@@^   .~                                      
+               ~BJ :@@@@@@@@BJYYYYYYJJJJYJJJJJJJ?!.     .!?JYYYJJYYYJJJYYYYY&@@@@@@@P7: .G#?.                                   
+            .?BG^  &@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?...5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@?  .Y#5:                                 
+          :5BJ.    @@@@@@@@@&PJJJJJJ????JJJJ5&@@@@@@@@@@@@@@@GYJJ??????JJJJJYB@@@@@@@@@Y     7BB~                               
+        !GG~       .YGG@@@@B        ...       G@@@@@@@@@@@@@:     .::.        !@@@@&GP7        ^G#?                             
+     .JB5:             &@@@Y      4P..P@G.    7@@@@@@@@@@@@&    ^:^B@@@P.      @@@@~             .J#5.                          
+     J@J               &@@@5     G@@4Y&@@&    ?@@@@?::^#@@@&   ~@4^B@@@@#     .@@@@~              ^#&:                          
+      .5#J.            &@@@5     ?@@@@@@@P    7@@@@.   ?@@@&   .@@@@@@@@Y     .@@@@~           .J#P:                            
+        .?#P:          #@@@G      .JGBBY:     5@@@&    ^@@@@.    ?B&&#P^      :@@@@~         ^PBJ.                              
+           ~BB!        7@@@@5.              :P@@@@J     #@@@&!.             .!&@@@&        7BG~                                 
+             :5#J.      ?@@@@@@@@@@@@@@@@@@@@@@@@J      .#@@@@@@&&&&&&&@&&@@@@@@@B.     :5BY.                                   
+               .J#!      .7G&&@@@@@@@@@@@@@@&&G7.         ^5#&@@@@@@@@@@@@@@@&BJ:       7!                                      
+
+
+ ''')
+
+print(f"**** WELCOME to States Flashcards Game {ver}   -by-    Dr.M-Dev ****")
+#====================#====================#====================#==================
+#====================#====================#====================#==================
